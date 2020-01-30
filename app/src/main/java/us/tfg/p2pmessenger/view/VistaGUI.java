9d79d588fff41403536b438f6a6e08df562e8cd3
lordcanete <pedcanvaz@gmail.com>
2020-01-30 21:07:19 +0100

    Creada clase para Vista GUI

---
+++ app/src/main/java/us/tfg/p2pmessenger/view/VistaGUI.java
@@ -0,0 +1,33 @@
+package us.tfg.p2pmessenger.view;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import rice.p2p.commonapi.Id;
+import us.tfg.p2pmessenger.controller.ControladorApp;
+import us.tfg.p2pmessenger.controller.ControladorConsolaImpl;
+import us.tfg.p2pmessenger.controller.GestorFicherosConsola;
+import us.tfg.p2pmessenger.model.Contacto;
+import us.tfg.p2pmessenger.model.Conversacion;
+import us.tfg.p2pmessenger.model.Grupo;
+import us.tfg.p2pmessenger.model.Mensaje;
+
+/**
+ * Created by PCANO on 30/01/20.
+ */
+public class VistaGUI implements Vista
+{
+    public VistaGUI()
+    {
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Funcionando Vista WEB\n");
+    }
+}
