8a9b7a0a2335c8945447f7fce1b12a4060c21afe
lordcanete <pedcanvaz@gmail.com>
2020-01-30 22:24:39 +0100

    Prueba inicial JavaFX

--- app/src/main/java/us/tfg/p2pmessenger/view/VistaGUI.java
+++ app/src/main/java/us/tfg/p2pmessenger/view/VistaGUI.java
@@ -17,6 +17,17 @@ import us.tfg.p2pmessenger.model.Conversacion;
 import us.tfg.p2pmessenger.model.Grupo;
 import us.tfg.p2pmessenger.model.Mensaje;
 
+
+import javafx.application.Application;
+import javafx.concurrent.Worker;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import netscape.javascript.JSObject;
+
+import java.io.File;
+import java.net.URL;
 /**
  * Created by PCANO on 30/01/20.
  */
@@ -26,9 +37,24 @@ public class VistaGUI implements Vista
     {
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = new File("./web/html/index.html").toURI().toURL();
+
+        WebView webView = new WebView();
+        final WebEngine webEngine = webView.getEngine();
+
+        Scene scene = new Scene(webView, 300, 150);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // now load the page
+        webEngine.load(url.toString());
+    }
+
     public static void main(String args[])
     {
-        System.out.println("Funcionando Vista WEB\n");
+        Application.launch(VistaGUI.class, args);
     }
 
     @Override
