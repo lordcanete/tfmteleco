58fff6233c18bea8da51e25d3b64c6fd27b3be10
lordcanete <pedcanvaz@gmail.com>
2020-02-25 20:27:57 +0100

    Modificado js para poder cerrar y abrir la ventana de agenda de contactos

--- app/src/main/java/us/tfg/p2pmessenger/view/web/js/appWindow.js
+++ app/src/main/java/us/tfg/p2pmessenger/view/web/js/appWindow.js
@@ -2,9 +2,11 @@ var idCapaAgenda ="#pagAppWindow_capaAgenda"
 
 function ocultarElemento(elementoId){
     $(elementoId).addClass("d-none");
+    $(elementoId).removeClass("d-flex");
 }
 
 function mostrarElemento(elementoId){
+    $(elementoId).addClass("d-flex");
     $(elementoId).removeClass("d-none");
 }
 
