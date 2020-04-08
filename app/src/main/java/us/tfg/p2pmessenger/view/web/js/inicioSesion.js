6d793c3b17cd81fa1c8d6d35da81ad372bccc776
lordcanete <pedcanvaz@gmail.com>
2020-04-08 21:11:53 +0200

    Agregada validacion formulario inicioSesion.js

--- app/src/main/java/us/tfg/p2pmessenger/view/web/js/inicioSesion.js
+++ app/src/main/java/us/tfg/p2pmessenger/view/web/js/inicioSesion.js
@@ -1,10 +1,15 @@
 var idBloque_notifError = "#pagInicioSesion_bloqueNotificacionError";
 var idBloque_textoError = "#pagInicioSesion_mensajeNotificacionError";
+var mensaje_validacionKO = "Por favor, proporcione un usuario y una contraseña"
 
 function pagInicioSesion_onClickAutenticar () {
     var usuario = $("#pagInicioSesion_formFieldUser").val();
-    var passwd = $("#pagInicioSesion_formFieldPass").val();    
-    javaConnector.iniciarSesion(usuario, passwd);
+    var passwd = $("#pagInicioSesion_formFieldPass").val();        
+    if(validarFormularioInicioSesion(usuario, passwd)){
+        javaConnector.iniciarSesion(usuario, passwd);
+    }else{
+        mostrarBloqueNotificacion(idBloque_notifError, idBloque_textoError, mensaje_validacionKO);
+    }
 };
 
 function pagInicioSesion_onClickRegistrar () {
@@ -12,4 +17,14 @@ function pagInicioSesion_onClickRegistrar () {
 };
 function pagInicioSesion_onClickCerrarNotificacionError(){
     ocultarBloqueNotificacion(idBloque_notifError);
+}
+
+function validarFormularioInicioSesion(usuario, passwd){
+    var validacion = false;
+    var usuarioTrim = usuario.trim();
+    var passwdTrim = passwd.trim();
+    if (!(usuario.trim() == "") && !(passwd.trim() == "")) {
+        validacion = true;
+    } 
+    return validacion;
 }
